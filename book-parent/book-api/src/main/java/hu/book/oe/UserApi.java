30ad0ba72f06528744351edd1162e242657a9f01
betti0888 <94833109+betti0888@users.noreply.github.com>
2021-12-21 08:32:40 +0100

    keycloak added

--- book-parent/book-api/src/main/java/hu/book/oe/UserApi.java
+++ book-parent/book-api/src/main/java/hu/book/oe/UserApi.java
@@ -3,6 +3,7 @@ package hu.book.oe;
 import java.util.Set;
 
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,18 +15,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/api")
 public interface UserApi {
     
+	@PreAuthorize("hasRole('USER_READER')")
     @GetMapping(value = "/User", produces = MediaType.APPLICATION_JSON_VALUE)
     Set<User> getAll();
 
+	@PreAuthorize("hasRole('USER_READER')")
     @GetMapping(value = "/User/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     User getById(@PathVariable(value = "id") String id);
 
+	@PreAuthorize("hasRole('USER_WRITER')")
     @PostMapping(value = "/User/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     User updateById(@PathVariable(value = "id") String id, @RequestBody User user);
 
+	@PreAuthorize("hasRole('USER_WRITER')")
     @PutMapping(value = "/User", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     User create(@RequestBody User user);
 
+	@PreAuthorize("hasRole('USER_WRITER')")
     @DeleteMapping(value = "/User/{id}")
     void delete(@PathVariable(value = "id") String id);
 
---
+++ book-parent/book-web/src/main/java/hu/book/oe/KeycloakTokenConverter.java
@@ -0,0 +1,7 @@
+package hu.book.oe;
+
+import java.util.Collection;
+
+public class KeycloakTokenConverter  {
+	
+    }
